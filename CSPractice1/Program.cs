4b5b532b45266ba0fd3420866389f70c797dd3c7
wlgus123 <mail30019794@gmail.com>
2024-06-27 12:04:17 +0900

    #1 12-8. var 키워드

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -86,6 +86,11 @@ namespace CSPractice1
             Console.WriteLine(++number);    // 12
             Console.WriteLine(number--);    // 12
             Console.WriteLine(--number);    // 10
+
+            // #1 12-8. var 키워드
+            var varnumber = 20;
+            //var abc;    // 반드시 초기화 해줘야 함
+
         }
     }
 }
