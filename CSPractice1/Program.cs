457d4c4ff74c6652bdb810a1fa974d284b2467c7
wlgus123 <mail30019794@gmail.com>
2024-06-27 12:19:10 +0900

    #3 14-2. while 반복문

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -123,8 +123,22 @@ namespace CSPractice1
                     break;
             }
 
-            // #2 13-3. 삼항 연산자
+            // #2 13-6. 삼항 연산자
             Console.WriteLine(number % 2 == 0 ? true : false);
+
+            // #3 14-1. 반복문과 배열
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray[99]);
+
+            // #3 14-2. while 반복문
+            int i = 0;
+            int[] intArray2 = { 52, 254, 23 };
+            while (i < intArray2.Length)
+            {
+                Console.WriteLine(intArray2[i]);
+                i++;    // 탈출을 위한 변수
+            }
+;
         }
     }
 }
